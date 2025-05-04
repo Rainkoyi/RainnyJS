f875ab71f82dd86f23f2b6828301f0b2b2a73cc8
cultist <75032462+dairycultist@users.noreply.github.com>
2025-05-04 19:57:04 -0400

    Update all.js

--- commands/BoobQuest/all.js
+++ commands/BoobQuest/all.js
@@ -1,19 +1,21 @@
 const { SlashCommandBuilder } = require("discord.js");
 
+// subcommands
+// https://docs.discordnet.dev/guides/int_basics/application-commands/slash-commands/subcommands.html
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("bq")
     .setDescription("Command for all interactions with BoobQuest.")
-    .addStringOption(option =>
-  		option.setName('testchoice')
-  			.setDescription('A test parameter.')
-  			.setRequired(true)
-  			.addChoices(
-  				{ name: 'Lemon', value: 'lemon' },
-  				{ name: 'Apple', value: 'apple' },
-  				{ name: 'Pear', value: 'pear' }
-			  )
-      ),
+    .addStringOption(option => option.setName('testchoice')
+		.setDescription('A test parameter.')
+		.setRequired(true)
+		.addChoices(
+			{ name: 'Lemon', value: 'lemon' },
+			{ name: 'Apple', value: 'apple' },
+			{ name: 'Pear', value: 'pear' }
+		)
+	),
   async execute(interaction) {
     await interaction.reply("You chose " + interaction.options.getString('testchoice') + ".");
   }
