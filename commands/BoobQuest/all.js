1203feb98481fbdc95b20379f0ccad192cc6a8bd
cultist <75032462+dairycultist@users.noreply.github.com>
2025-05-04 20:19:49 -0400

    Update all.js

--- commands/BoobQuest/all.js
+++ commands/BoobQuest/all.js
@@ -1,23 +1,41 @@
 const { SlashCommandBuilder } = require("discord.js");
 
-// tutorials
-// https://docs.discordnet.dev/guides/int_basics/application-commands/slash-commands/subcommands.html
+// tutorial
 // https://discordjs.guide/slash-commands/parsing-options.html#subcommands
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("bq")
     .setDescription("Command for all interactions with BoobQuest.")
-    .addStringOption(option => option.setName('testchoice')
-		.setDescription('A test parameter.')
-		.setRequired(true)
-		.addChoices(
-			{ name: 'Lemon', value: 'lemon' },
-			{ name: 'Apple', value: 'apple' },
-			{ name: 'Pear', value: 'pear' }
-		)
-	),
+    .addSubcommand(subcommand => subcommand
+			.setName('paycheck')
+			.setDescription('Get some money (cooldown: 1h).')
+    )
+    .addSubcommand(subcommand => subcommand
+			.setName('buyfruit')
+			.setDescription('Test subcommand.')
+     //  .addStringOption(option => option
+     //    .setName('fruit')
+     //    .setDescription('Test parameter.')
+    	// 	.setRequired(true)
+    	// 	.addChoices(
+    	// 		{ name: 'Lemon', value: 'lemon' },
+    	// 		{ name: 'Apple', value: 'apple' },
+    	// 		{ name: 'Pear', value: 'pear' }
+    	// 	)
+    	// )
+    )
+  ,
   async execute(interaction) {
-    await interaction.reply("You chose " + interaction.options.getString('testchoice') + ".");
+
+    switch(interaction.options.getSubcommand()) {
+      case "paycheck":
+        await interaction.reply("You chose paycheck.");
+        break;
+      case "buyfruit":
+        await interaction.reply("You chose buyfruit.");
+        // await interaction.reply("You chose " + interaction.options.getString('testchoice') + ".");
+        break;
+    }
   }
 };
