358c30409d66d3ed1b007e4579aeaa0295147913
cultist <75032462+dairycultist@users.noreply.github.com>
2025-05-04 23:35:00 -0400

    Update all.js

--- commands/BoobQuest/all.js
+++ commands/BoobQuest/all.js
@@ -1,10 +1,18 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+const exampleEmbed = new EmbedBuilder()
+	.setColor(0x0099FF)
+	.setTitle("Uzaki Hana")
+	// .setDescription('Some description here')
+	.setThumbnail("https://i.pinimg.com/236x/9d/0f/a3/9d0fa3da5c7d68f800baf1dcbbb76e70.jpg")
+	.addFields(
+		{ name: "Cup Size", value: "DD (:coin:+11/paycheck)", inline: true },
+		{ name: "From", value: "Uzaki-chan Wants to Hang Out!", inline: true },
+	)
+	.setFooter({ text: "BoobQuest" });
 
 // tutorial
 // https://discordjs.guide/slash-commands/parsing-options.html#subcommands
-
-// https://i.pinimg.com/236x/9d/0f/a3/9d0fa3da5c7d68f800baf1dcbbb76e70.jpg
-// Cup Size: DD (:coin:+11/paycheck)
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -32,15 +40,20 @@ module.exports = {
         )
       )
     )
+    .addSubcommand(subcommand => subcommand
+      .setName("testembed")
+      .setDescription("Test waifu embeds.")
+    )
   ,
   async execute(interaction) {
 
     switch(interaction.options.getSubcommand()) {
+        
       case "paycheck":
         await interaction.reply("You chose paycheck.");
         break;
+        
       case "roulette":
-
         let betColor = interaction.options.getString("color");
         let resultColor = Math.floor(Math.random() * 2) == 0 ? "red" : "black";
 
@@ -50,6 +63,10 @@ module.exports = {
           await interaction.reply("You bet on " + betColor + ", but it's " + resultColor + "! You lost 🪙" + interaction.options.getInteger("wager") + " (remaining balance: $0).");
         }
         break;
+
+        case "testembed":
+          interaction.channel.send({ embeds: [exampleEmbed] });
+          break;
     }
   }
 };
